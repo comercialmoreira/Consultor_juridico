0fc56f78285947ca3d6adc5da7ac96376a0c2cd5
Felipe <felipe@MacBook-Air-de-Felipe.local>
2024-11-14 18:41:42 -0300

    App

---
+++ App.py
@@ -0,0 +1,167 @@
+import tempfile
+import streamlit as st
+from langchain.memory import ConversationBufferMemory
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+from Loaders import *
+
+
+groq = st.secrets["GROQ_API_KEY"]
+
+TIPOS_ARQUIVOS_VALIDOS = [
+    'Gerador de Contratos', 'Analisador de Contratos'
+]
+
+CONFIG_MODELOS = {'Groq': 
+                        {'modelos': ['llama-3.1-70b-versatile', 'gemma2-9b-it', 'mixtral-8x7b-32768'],
+                         'chat': ChatGroq,
+                         'api_key': groq}
+}
+
+
+MEMORIA = ConversationBufferMemory()
+
+def carrega_arquivos(tipo_arquivo, arquivo):
+    if tipo_arquivo == 'Gerador de Contratos':
+        documento = carrega_site(arquivo)
+    if tipo_arquivo == 'Analisador de Contratos':
+        with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as temp:
+            temp.write(arquivo.read())
+            nome_temp = temp.name
+        documento = carrega_pdf(nome_temp)
+    return documento
+
+def carrega_modelo(provedor, modelo, api_key, tipo_arquivo, arquivo):
+
+    documento = carrega_arquivos(tipo_arquivo, arquivo)
+
+    system_message = '''
+Objetivo principal: Auxiliar profissionais da advocacia nas áreas de análise de contratos e geração de contratos, 
+de forma formal, profissional e embasada nas diretrizes da Ordem dos Advogados do Brasil (OAB).
+
+Funções Disponíveis:
+
+1. **Análise de Contratos:**
+   - **Descrição:** Revisar contratos fornecidos pelo usuário, analisando cláusulas, identificando lacunas (gaps), inconsistências, 
+     erros ou potenciais riscos jurídicos.
+   - **Abordagem:** Basear a análise nas normativas da OAB e na legislação brasileira aplicável.
+   - **Como funciona:**
+     - Ler detalhadamente o contrato fornecido.
+     - Fornecer uma análise detalhada por seção ou cláusula.
+     - Apontar sugestões de melhorias, omissões ou termos que podem ser problemáticos.
+     - Perguntar ao usuário o contexto ou objetivo do contrato, se necessário.
+
+2. **Geração de Contratos:**
+   - **Descrição:** Auxiliar na redação de contratos robustos e claros, ajustados às necessidades específicas do usuário e das partes envolvidas.
+   - **Abordagem:**
+     - Formular perguntas estratégicas para compreender o escopo, as partes envolvidas, os interesses e os riscos.
+     - Garantir que o contrato inclua cláusulas essenciais, personalizadas para o contexto fornecido.
+     - Propor redações claras e técnicas, embasadas nas normas da OAB.
+   - **Como funciona:**
+     - Elaborar um contrato completo ou revisar um já iniciado.
+     - Garantir a inclusão de cláusulas obrigatórias e estratégicas.
+
+### Diretrizes para Interação
+
+1. **Início da Conversa:**
+   - Cumprimentar formalmente e entender, de acordo com o tipo de arquivo fornecido, qual das duas funções será exercida.
+   - Solicitar detalhes sobre o contrato, seja para análise ou geração.
+
+2. **Durante a Resposta:**
+
+   - **Se for análise de contratos:**
+     - Examinar cláusula por cláusula do documento.
+     - Identificar potenciais falhas ou lacunas.
+     - Propor ajustes com base na legislação e nas boas práticas.
+
+   - **Se for geração de contratos:**
+     - Fazer perguntas claras e diretas para entender o escopo, como:
+       - Quais as partes envolvidas?
+       - Qual o objeto do contrato?
+       - Qual a duração e as condições de rescisão?
+       - Existem obrigações ou penalidades específicas?
+     - Utilizar as respostas para redigir um contrato completo e bem estruturado.
+
+3. **Estilo e Formatação:**
+   - Usar **markdown** para estruturar a resposta.
+   - Destacar palavras importantes com **negrito**.
+   - Utilizar listas para pontos importantes.
+   - Garantir clareza, precisão e organização nas respostas.
+   - Citar artigos ou normas jurídicas aplicáveis quando pertinente.
+
+### Limitações:
+- Não inventar informações ou criar cláusulas sem base legal.
+- Não fazer sugestões ou recomendações sem base na legislação.
+- Não responder a ofensas ou interações de má-fé.
+
+'''.format(tipo_arquivo, documento)
+
+
+    template = ChatPromptTemplate.from_messages([
+        ('system', system_message),
+        ('placeholder', '{chat_history}'),
+        ('user', '{input}')
+    ])
+    chat = CONFIG_MODELOS[provedor]['chat'](model=modelo, api_key=api_key)
+    chain = template | chat
+
+    st.session_state['chain'] = chain
+
+def pagina_chat():
+    st.header('Consultor :violet[Juridico]', divider='violet')
+
+    chain = st.session_state.get('chain')
+    if chain is None:
+        st.error('Carrege o assistente na sidebar antes de usar o chat')
+        st.stop()
+
+    memoria = st.session_state.get('memoria', MEMORIA)
+    for mensagem in memoria.buffer_as_messages:
+        chat = st.chat_message(mensagem.type)
+        chat.markdown(mensagem.content)
+
+    input_usuario = st.chat_input('Digite para o seu assistente')
+    if input_usuario:
+        chat = st.chat_message('human')
+        chat.markdown(input_usuario)
+
+        chat = st.chat_message('ai')
+        resposta = chat.write_stream(chain.stream({
+            'input': input_usuario, 
+            'chat_history': memoria.buffer_as_messages
+            }))
+        
+        memoria.chat_memory.add_user_message(input_usuario)
+        memoria.chat_memory.add_ai_message(resposta)
+        st.session_state['memoria'] = memoria
+
+def sidebar():
+    tabs = st.tabs(['Upload de Arquivos', 'Seleção de Modelos'])
+    with tabs[0]:
+        tipo_arquivo = st.selectbox('Selecione o tipo de arquivo', TIPOS_ARQUIVOS_VALIDOS)
+        if tipo_arquivo == 'Analisador de Contratos':
+            arquivo = st.file_uploader('Faça o upload do arquivo pdf', type=['.pdf'])
+        if tipo_arquivo == 'Gerador de Contratos':
+            arquivo = "https://wolfadvocacia.com.br"
+       
+
+    with tabs[1]:
+        provedor = st.selectbox('Selecione o provedor dos modelo', CONFIG_MODELOS.keys())
+        modelo = st.selectbox('Selecione o modelo', CONFIG_MODELOS[provedor]['modelos'])
+        api_key = CONFIG_MODELOS[provedor]['api_key']
+        
+    if st.button('Inicializar Assistente', use_container_width=True):
+            carrega_modelo(provedor, modelo, api_key, tipo_arquivo, arquivo)
+    if st.button('Apagar Histórico de Conversa', use_container_width=True):
+            st.session_state['memoria'] = MEMORIA
+
+def main():
+    with st.sidebar:
+        sidebar()
+    pagina_chat()
+
+
+if __name__ == '__main__':
+    main()
